e93b825f7944d154f2d24c82f9b93d2abb39be04
Kaito-Fujihira <k.f.alamoana@gmail.com>
2020-10-09 18:56:28 +0900

    add

---
+++ JavaScript/app.js
@@ -0,0 +1,89 @@
+var num1 = 63;
+var num2 = 43;
+var num3 = 45;
+var num4 = 4;
+var num5 = 12;
+alert(num1 + num2 - (num3 * num4) / num5);
+
+var alertString;
+alertString = addString("WebCamp");
+
+alert(alertString);
+
+function addString(strA) {
+  var addStr = "Hello " + strA;
+  return addStr;
+}
+
+var user_hand = prompt(
+  "じゃんけんの手をグー、チョキ、パーから選んでください。"
+);
+while (
+  user_hand != "グー" &&
+  user_hand != "チョキ" &&
+  user_hand != "パー" &&
+  user_hand != null
+) {
+  alert("グー・チョキ・パーのいずれかを入力して下さい");
+  user_hand = prompt("じゃんけんの手をグー、チョキ、パーから選んでください。");
+}
+var js_hand = getJShand();
+var judge = winLose(user_hand, js_hand);
+if (user_hand != null) {
+  alert(
+    "あなたの選んだ手は" +
+      user_hand +
+      "です。\nJavaScriptの選んだ手は" +
+      js_hand +
+      "です。\n結果は" +
+      judge +
+      "です。"
+  );
+} else {
+  alert("またチャレンジしてね");
+}
+
+function getJShand() {
+  var js_hand_num = Math.floor(Math.random() * 3);
+  var hand_name;
+
+  if (js_hand_num == 0) {
+    hand_name = "グー";
+  } else if (js_hand_num == 1) {
+    hand_name = "チョキ";
+  } else if (js_hand_num == 2) {
+    hand_name = "パー";
+  }
+  return hand_name;
+}
+
+function winLose(user, js) {
+  var winLoseStr;
+
+  if (user == "グー") {
+    if (js == "グー") {
+      winLoseStr = "あいこ";
+    } else if (js == "チョキ") {
+      winLoseStr = "勝ち";
+    } else if (js == "パー") {
+      winLoseStr = "負け";
+    }
+  } else if (user == "チョキ") {
+    if (js == "グー") {
+      winLoseStr = "負け";
+    } else if (js == "チョキ") {
+      winLoseStr = "あいこ";
+    } else if (js == "パー") {
+      winLoseStr = "勝ち";
+    }
+  } else if (user == "パー") {
+    if (js == "グー") {
+      winLoseStr = "勝ち";
+    } else if (js == "チョキ") {
+      winLoseStr = "負け";
+    } else if (js == "パー") {
+      winLoseStr = "あいこ";
+    }
+  }
+  return winLoseStr;
+}
